f0e048ab5c86dafe3a10aefbbf9ff6dd640c9f66
Damian Gola <gola@imbs.uni-luebeck.de>
2019-06-27 11:13:53 +0200

    simplify init in this sapu specific branch

--- init.R
+++ init.R
@@ -7,18 +7,7 @@ system <- Sys.info()["nodename"]
 home <- Sys.getenv("HOME")
 project_name <- "1kg_phase3"
 work_dirs <- list(
-  n01 = list(
-    gola = "Documents/Work"
-  ),
-  n04 = list(
-    gola = "Documents/Work"
-  ),
-  gola.local = list(
-    gola = "Documents/Work"
-  ),
-  damian.novalocal = list(
-    damian = "documents"
-  )
+  damian = "documents"
 )
 main_dir <- file.path(home,
                       work_dirs[[system]][[user]],
@@ -29,32 +18,11 @@ dir.create(scripts_dir <- file.path(main_dir, "scripts"),
 dir.create(report_dir <- file.path(main_dir, "report"),
            recursive = TRUE, showWarnings = FALSE)
 
-input_dir <- switch(
-  system,
-  damian.novalocal = "/data",
-  "/imbs/external_data/annotation_and_references/"
-)
-input_dir_1kg_phase3 <- switch(
-  system,
-  damian.novalocal = file.path(input_dir, "1000G", "release", "20130502"),
-  file.path(input_dir,
-            "hapmap",
-            "1000G",
-            "1000GP_Phase3")
-)
-input_dir_human_ref <- switch(
-  system,
-  damian.novalocal = file.path(input_dir, "GRCh37REF"),
-  file.path(input_dir,
-            "human-ref-genomes",
-            "b37")
-)
+input_dir <- "/data"
+input_dir_1kg_phase3 <- file.path(input_dir, "1000G", "release", "20130502")
+input_dir_human_ref <- file.path(input_dir, "GRCh37REF")
 dir.create(
-  path = project_dir <- switch(
-    system,
-    damian.novalocal = file.path(input_dir_1kg_phase3, "converted"),
-    "/imbs/projects/1kg_phase3"
-  ),
+  path = project_dir <- file.path(input_dir_1kg_phase3, "converted"),
   recursive = TRUE, showWarnings = FALSE
 )
 
@@ -84,23 +52,8 @@ dir.create(
 ##============================================================================+
 ## external software ----
 ##============================================================================+
-bcftools_exec <- switch(system,
-                        "damian.novalocal" = "/home/damian/bin/bcftools1.9",
-                        "/software/bin/bcftools1.6"
-)
-genotype_harmonizer_exec <- switch(system,
-                                   "damian.novalocal" = "/home/damian/bin/GenotypeHarmonizer1.4.20",
-                                   "/software/bin/GenotypeHarmonizer1.4.20"
-)
-plink_exec <- switch(system,
-                     "damian.novalocal" = "/home/damian/bin/plink1.90b6.10",
-                     "/software/bin/plink1.9b5"
-)
-plink2_exec <- switch(system,
-                      "damian.novalocal" = "/home/damian/bin/plink2.0a20190617",
-                      "/software/bin/plink2a20190527"
-)
-gcta_exec <- switch(system,
-                    "damian.novalocal" = "/home/damian/bin/gcta1.92.2b",
-                    "/software/bin/gcta1.26.0"
-)
+bcftools_exec <- "/home/damian/bin/bcftools1.9"
+genotype_harmonizer_exec <- "/home/damian/bin/GenotypeHarmonizer1.4.20"
+plink_exec <- "/home/damian/bin/plink1.90b6.10"
+plink2_exec <- "damian.novalocal" = "/home/damian/bin/plink2.0a20190617"
+gcta_exec <- "/home/damian/bin/gcta1.92.2b"
